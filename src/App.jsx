156173006f95e9fedb95b05d6087a3f4f4155218
pilatti40-debug <pilatti40@gmail.com>
2026-05-27 00:06:18 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Meu App Funcionando 🚀
+      </h1>
+    </div>
+  )
+}

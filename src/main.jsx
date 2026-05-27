546532e628e3a05c833af3ad62c0199319827cd1
pilatti40-debug <pilatti40@gmail.com>
2026-05-27 07:56:26 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import App from './NewArService.jsx'; // Certifique-se de que o nome do ficheiro coincide
+import './index.css'; // Certifique-se de que este ficheiro existe
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
